6d19659a6430dcb878b2ca94057d4f5c70edaf02
jingong <jiajingong@163.com>
2017-04-06 19:33:24 +0800

    更改变量名

--- app/src/main/java/com/edu/ldu/cn/mynews/NewsDetailActivity.java
+++ app/src/main/java/com/edu/ldu/cn/mynews/NewsDetailActivity.java
@@ -10,17 +10,17 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class NewsDetailActivity extends Activity {
-    private WebView wb;
+    private WebView wv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_detail);
         Intent intent = getIntent();
         final String url = intent.getStringExtra("url");
-        wb = (WebView)findViewById(R.id.wb);
-        wb.getSettings().setDefaultTextEncodingName("UTF-8");
-        wb.loadUrl(url);
-        wb.setWebViewClient(new WebViewClient(){
+        wv = (WebView)findViewById(R.id.wv);
+        wv.getSettings().setDefaultTextEncodingName("UTF-8");
+        wv.loadUrl(url);
+        wv.setWebViewClient(new WebViewClient(){
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
                 view.loadUrl(url);
